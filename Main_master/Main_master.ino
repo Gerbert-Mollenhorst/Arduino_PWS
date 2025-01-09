feff5243df2c0ec77b9d99f7daf497ad81f042f0
Gerbert <gerbertmollenhorst@protonmail.com>
2025-01-09 18:18:24 +0100

    v1.3

--- Main_master/Main_master.ino
+++ Main_master/Main_master.ino
@@ -29,8 +29,10 @@ void setup() {
   pinMode(side_road , INPUT_PULLUP);
   // start lights
   write(11);
-  write(21);
+  write(23);
   write(33);
+  write(43);
+  write(53);
   // debug tool
   Serial.println("end setup");
 }
@@ -41,49 +43,89 @@ void loop() {
   unsigned long currenttime = millis();
   if (ident == 1 && currenttime - prevtime > green) { // to yellow
     write(12);
-    write(22);
     ident = 2;
     prevtime = currenttime;
   }
-  
   if (ident == 2 && currenttime - prevtime > yellow) {// to red
     write(13);
-    write(23);
     ident = 3;
     prevtime = currenttime;
   }
-
   if (ident == 3 && currenttime - prevtime > red ) {// to green
-    write(31);
+    write(21);
     ident = 4;
     prevtime = currenttime;
   }
-
   if (ident == 4 && currenttime - prevtime > green) {// to yellow
-    write(32);
+    write(22);
     ident = 5;
     prevtime = currenttime;
   }
-
   if (ident == 5 && currenttime - prevtime > yellow) {// to red
-    write(33);
+    write(23);
     ident = 6;
     prevtime = currenttime;
   }
-
   if (ident == 6 && currenttime - prevtime > red) {// to green
+    write(31);
+    ident = 7;
+    prevtime = currenttime;
+  }
+  if (ident == 7 && currenttime - prevtime > green) {// to green
+    write(32);
+    ident = 8;
+    prevtime = currenttime;
+  }
+  if (ident == 8 && currenttime - prevtime > yellow) {// to green
+    write(33);
+    ident = 9;
+    prevtime = currenttime;
+  }
+  if (ident == 9 && currenttime - prevtime > red) {// to green
+    write(41);
+    ident = 10;
+    prevtime = currenttime;
+  }
+  if (ident == 10 && currenttime - prevtime > green) {// to green
+    write(42);
+    ident = 11;
+    prevtime = currenttime;
+  }
+  if (ident == 11 && currenttime - prevtime > yellow) {// to green
+    write(43);
+    ident = 12;
+    prevtime = currenttime;
+  }
+  if (ident == 12 && currenttime - prevtime > red) {// to green
+    write(51);
+    ident = 13;
+    prevtime = currenttime;
+  }
+  if (ident == 13 && currenttime - prevtime > green) {// to green
+    write(52);
+    ident = 14;
+    prevtime = currenttime;
+  }
+  if (ident == 14 && currenttime - prevtime > yellow) {// to green
+    write(53);
+    ident = 15;
+    prevtime = currenttime;
+  }
+  
+  if (ident == 15 && currenttime - prevtime > red) {// to green
     write(11);
-    write(21);
     ident = 1;
     prevtime = currenttime;
   }
 
+/*
   if (ident == 1 && digitalRead(side_road) == 0 ){
     prevtime = currenttime + (yellow - 1000);
   }
 
   if (ident == 4 && digitalRead(main_road) == 0 ){
     prevtime = currenttime + (yellow - 1000);
-  }
+  } 
+*/ 
 }
 
